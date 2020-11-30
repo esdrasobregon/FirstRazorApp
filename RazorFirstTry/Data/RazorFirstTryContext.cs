b99be647738a732534e561cf8aa72fec783eabc2
esdras jared obregon ubeda <esdrasobregon@hotmail.com>
2020-11-29 22:48:46 -0600

    first step

---
+++ RazorFirstTry/Data/RazorFirstTryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorFirstTry.Models;
+
+namespace RazorFirstTry.Data
+{
+    public class RazorFirstTryContext : DbContext
+    {
+        public RazorFirstTryContext (DbContextOptions<RazorFirstTryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorFirstTry.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorFirstTry/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorFirstTry.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
